9713776037754ce2c30ce7b128f02fed9f51d184
Muhammad Danish Aqmar Bin Rushdan <72965748+b2200356806@users.noreply.github.com>
2023-04-28 14:56:16 +0300

    Delete ICES4HU directory

--- ICES4HU/src/main/java/penguins/backend/Semester/Exception/SemesterNotStartedException.java
+++
@@ -1,7 +0,0 @@
-package penguins.backend.Semester.Exception;
-
-public class SemesterNotStartedException extends RuntimeException {
-    public SemesterNotStartedException(String message) {
-        super(message);
-    }
-}
